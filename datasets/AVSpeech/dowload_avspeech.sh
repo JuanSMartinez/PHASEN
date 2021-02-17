b8a10af4f6087dd046a91629fd130ceb88134b56
JuanSMartinez <mart1304@purdue.edu>
2021-02-17 09:59:16 -0500

    added shell script to download AVSpeech

---
+++ datasets/AVSpeech/dowload_avspeech.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -d train ]
+then 
+	echo "Training directory already exists. Would you like to erase it or keep its contents? (erase/keep):"
+	read train_choice
+	while [ ! -z $train_choice ]
+	do
+	if [ $train_choice == "erase" ]
+	then 
+		rm -rf train
+		mkdir train
+		echo "Erased all previous training files and created a new empty training directory."
+		break
+	elif [ $train_choice == "keep" ]
+	then
+		echo "Keeping files inside the train directory."
+		break
+	else
+		echo "Invalid choice. Please choose to erase or keep:"
+		read train_choice
+	fi
+	done
+else
+	mkdir train
+	echo "Created empty training directory."
+fi
+
+TRAIN_CSV=id_files/avspeech_train.csv
+while IFS=, read -r id start end x y
+do
+	echo $id
+done < $TRAIN_CSV
