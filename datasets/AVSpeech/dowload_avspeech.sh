a8290097005ede9cae01526b0c6f507bfcbfb5c5
JuanSMartinez <mart1304@purdue.edu>
2021-02-19 14:56:44 -0500

    Finished code to download all training files for AVSpeech

--- datasets/AVSpeech/dowload_avspeech.sh
+++ datasets/AVSpeech/dowload_avspeech.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+Download_Training_Set(){
+	# The CSV files with video ids must have been downloaded in advance
+	# Downloading the files via wget or curl proved to be extremely difficult (requires Google authentication) and not worth the effort
+
+	TRAIN_CSV="avspeech_train.csv"
+	TEST_CSV="avspeech_test.csv"
+	PREFIX="http://youtube.com/watch?v="
+
+	SUCCESS_TRAIN=0
+	FAIL_TRAIN=0
+	TOTAL_TRAIN=0
+	if [ -f "$TRAIN_CSV" ]
+	then
+		while IFS=, read -r ID START END x y
+		do
+			((TOTAL_TRAIN = TOTAL_TRAIN + 1))
+			# Try to download and get the speech of a video	
+			OUTPUT="train/_$ID.%(ext)s"
+			URL="$PREFIX$ID"
+			if youtube-dl -x --audio-format "wav" --audio-quality 0 -o "$OUTPUT" "$URL"
+			then
+				ffmpeg -nostdin -hide_banner -loglevel error -i "train/_$ID.wav" -ss "$START" -to "$END" -c copy "train/$ID.wav"
+				rm "train/_$ID.wav"
+				((SUCCESS_TRAIN = SUCCESS_TRAIN + 1))
+			else
+				((FAIL_TRAIN = FAIL_TRAIN + 1))
+			fi
+		done < "$TRAIN_CSV"
+		echo "Total Files: $TOTAL_TRAIN. Succesfully downloaded and processed: $SUCCESS_TRAIN. Failed to download: $FAIL_TRAIN" > log_train.txt
+	else
+		echo "ERROR: Metadata for training files not found. Download the file 'avspeech_train.csv', place it along this script and run again."
+		exit -1
+	fi
+}
+
+
 if [ -d train ]
 then 
 	echo "Training directory already exists. Would you like to erase it or keep its contents? (erase/keep):"
@@ -11,6 +47,7 @@ then
 		rm -rf train
 		mkdir train
 		echo "Erased all previous training files and created a new empty training directory."
+		Download_Training_Set
 		break
 	elif [ "$train_choice" == "keep" ]
 	then
@@ -26,18 +63,4 @@ else
 	echo "Created empty training directory."
 fi
 
-# The CSV files with video ids must have been downloaded in advance
-# Downloading the files via wget or curl proved to be extremely difficult (requires Google authentication) and not worth the effort
 
-TRAIN_CSV="avspeech_train.csv"
-TEST_CSV="avspeech_test.csv"
-if [ -f "$TRAIN_CSV" ]
-then
-	while IFS=, read -r id start end x y
-	do
-		echo $id
-	done < "$TRAIN_CSV"
-else
-	echo "ERROR: Metadata for training files not found. Download the file 'avspeech_train.csv', place it along this script and run again."
-	exit -1
-fi
