92dae0dffb256c83de27614b58b0cc0dd7ce08fa
JuanSMartinez <mart1304@purdue.edu>
2021-03-11 16:55:33 -0500

    Added another sequential module to FTB

--- phasen.py
+++ phasen.py
@@ -11,13 +11,18 @@ class FTB(nn.Module):
     The FTB module as described in the original paper by Yin et al., (2020)
     '''
     
-    def __init__(self, Ca=96, Cr=5):
+    def __init__(self, Ca=96, Cr=5, T=301, F=258):
         super(FTB, self).__init__()
 
         # Sub attention module
-        self.att_mod = nn.Sequential(
+        self.conv_att = nn.Sequential(
                 nn.Conv2d(Ca, Cr, kernel_size=(1,1), stride=1, dilation=1, padding=0),
                 nn.BatchNorm2d(Cr),
+                nn.ReLU()
+                )
+        self.conv_att_1d = nn.Sequential(
+                nn.Conv1d(1, 1, kernel_size=9, stride=1, dilation=1, padding=4),
+                nn.BatchNorm1d(1),
                 nn.ReLU()
                 )
 
